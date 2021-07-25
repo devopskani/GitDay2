82b56984d7ec788d94604a05cc5414cf6e27ca7c
unknown <kanivision@gmail.com>
2021-07-25 12:51:26 +0530

    folder structure

---
+++ Sample.java
@@ -0,0 +1,23 @@
+package org.sample;
+
+public class Sample {
+	private void empName() {
+		System.out.println("kani");
+
+	}
+	private void empAddress() {
+		System.out.println("chennai");
+
+	}
+	private void empphone() {
+		System.out.println("123456789");
+
+	}
+	public static void main(String[] args) {
+		Sample s = new Sample();
+		s.empName();
+		s.empAddress();
+		s.empphone();
+	}
+
+}
